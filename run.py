20f8d764f3d1e14ebdd3712845bcd79cc9579267
王松-pc <719547997@qq.com>
2022-09-05 23:30:38 +0800

    也不知道要怎么把网络层解耦

--- run.py
+++ run.py
@@ -2,13 +2,35 @@
 主文件。入口
 wangsong2 2022.8.31 (8月过得不错！）
 """
+import numpy as np
+import torch
 
 from config import set_args
 from dataloader.task_manage import TaskManage
 
+from utils import timer
+
 args = set_args()
+
+# Seed
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(args.seed)
+else:
+    print('[CUDA unavailable]'); exit()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
 
 task_manage = TaskManage(args)
 task_manage.get_tasklist(args.task_list)
-task_manage.load_data()
+with timer('Load task list'):
+    task_manage.load_data()
+
+print('Inits...')
+
+
+
+
+
 print('done')
