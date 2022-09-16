6c5fa2e05c6c09fd4d4ff41f40c68e89e8664c7b
Kuroneko <719547997@qq.com>
2022-09-16 06:57:57 +0000

    Update utils.py
    增加了显卡和现存显示

--- utils.py
+++ utils.py
@@ -26,6 +26,18 @@ def timer(name):
 
 def get_available_gpus(order='first', limit=1, maxLoad=0.5, maxMemory=0.5, memoryFree=0, includeNan=False, excludeID=[],
                        excludeUUID=[]):
+    """
+
+    :param order:first,last,random,load,memory
+    :param limit:返回GPU的数量
+    :param maxLoad:最大负载比（0~1）
+    :param maxMemory:最大内存占用（0~1）
+    :param memoryFree:空闲内存
+    :param includeNan:GPU负载和内存占用为nan的情况
+    :param excludeID:需要排除的gpu id
+    :param excludeUUID:需要排除的gpu uuid
+    :return:List of available gpu ids.
+    """
     return GPUtil.getAvailable(order=order, limit=limit, maxLoad=maxLoad, maxMemory=maxMemory, memoryFree=memoryFree,
                                includeNan=includeNan, excludeID=excludeID, excludeUUID=excludeUUID)
 
@@ -39,18 +51,26 @@ class GPUMonitor(Thread):
         self.status = {}
         self.ids = gpu_ids
         self.start_time = time.time()
+        gpus = GPUtil.getGPUs()
 
         for gpu_id in self.ids:
-            self.status.update({f'gpu:{gpu_id}': {'memory': [], 'load': []}})
+            self.status.update({f'gpu:{gpu_id}':
+                                    {'memory': [],
+                                     'load': [],
+                                     'name':gpus[gpu_id].name,
+                                     'memoryTotal':gpus[gpu_id].memoryTotal}
+                                })
+            self.status[f'gpu:{gpu_id}']['memory'].append(gpus[gpu_id].menoryUsed)
+            self.status[f'gpu:{gpu_id}']['load'].append(gpus[gpu_id].load)
 
     def run(self):
         while not self.stopped:
             # see detail at https://github.com/anderskm/gputil/blob/master/GPUtil/GPUtil.py
+            time.sleep(self.delay)
             gpus = GPUtil.getGPUs()
             for gpu_id in self.ids:
-                self.status[f'gpu:{gpu_id}']['memory'].append(gpus[gpu_id].memoryUsed)
+                self.status[f'gpu:{gpu_id}']['memory'].append(gpus[gpu_id].menoryUsed)
                 self.status[f'gpu:{gpu_id}']['load'].append(gpus[gpu_id].load)
-            time.sleep(self.delay)
 
     def stop(self):
         self.stopped = True
